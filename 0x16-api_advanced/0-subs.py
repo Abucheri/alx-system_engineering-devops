fd158720b6b3b81676c9b0026a50bcabb5f988e8
O'Brien <abucheriobrien@gmail.com>
2024-02-13 22:55:32 +0300

    How many subs?_0_update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,11 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    # Set a custom User-Agent
-    headers = {'User-Agent': 'Python/requests'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Abucheri)"}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        data = response.json()
-        return data['data']['subscribers']
-    except (requests.RequestException, KeyError):
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
