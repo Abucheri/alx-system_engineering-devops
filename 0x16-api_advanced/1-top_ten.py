3c81fe2bc1140d0cd215e6be6d4c8ac75ee4a3eb
O'Brien Abucheri <abucheriobrien@gmail.com>
2024-02-15 21:26:35 +0300

    Update task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,21 +11,17 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts listed for a given subreddit.
     If the subreddit is invalid, prints None.
     """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'MyApp/1.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    try:
-        # Raise an HTTPError for bad responses
-        response.raise_for_status()
-        data = response.json().get('data').get('children')
-
-        for text in range(10):
-            print(data[text].get('data').get('title'))
-    except requests.exceptions.HTTPError:
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Abucheri)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
-    except requests.exceptions.RequestException:
-        print("None")
-    except (ValueError, AttributeError):
-        print("None")
+        return
+    data = response.json().get("data")
+    [print(text.get("data").get("title")) for text in data.get("children")]
