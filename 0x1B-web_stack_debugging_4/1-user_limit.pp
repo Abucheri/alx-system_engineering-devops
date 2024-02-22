83339d6b5ca46d473c331dfb0a965fd8eda1e8fc
O'Brien <abucheriobrien@gmail.com>
2024-02-22 15:29:36 +0000

    User limit_update0

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,12 @@
 # Puppet manifest to change OS configuration for holberton user
 
-# Change the open files limit for the holberton user
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 4096',
-  user    => 'holberton',
-  path    => '/usr/local/bin/:/bin/',
+# Set open files limit for holberton user
+limits { 'holberton_open_files':
+  ensure   => 'present',
+  domain   => 'holberton',
+  type     => 'hard',
+  item     => 'nofile',
+  value    => '4096',
+  apply_to => 'all',
+  provider => 'pam',
 }
